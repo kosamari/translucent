27a59b706678da9d95bdd581f7b2e5c0627cc7ef
kosamari <mariko.kosaka@gmail.com>
2016-04-19 16:07:16 +0100

    change app to regular window

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 var app = require('app')
 var BrowserWindow = require('browser-window')
-var Tray = require('tray')
-var nativeImage = require('native-image')
 var mainWindow = null
 
 app.on('ready', function () {
   mainWindow = new BrowserWindow({
     'width': 270,
     'height': 400,
-    'frame': false,
-    'show': false,
-    'skip-taskbar': false
+    'frame': true,
+    'show': true
   })
-
   mainWindow.loadUrl('file://' + __dirname + '/translucent.html')
+})
 
-  mainWindow.on('blur', function () {
-    mainWindow.hide()
-  })
-
-  var trayIcon = new Tray(nativeImage.createFromPath(__dirname + '/icon.png'))
-
-  trayIcon.setToolTip(app.getName())
-
-  trayIcon.on('clicked', function (e, b) {
-    mainWindow.setPosition(b.x - 130, 0)
-    if (mainWindow.isVisible()) {
-      mainWindow.hide()
-      mainWindow.reload()
-    } else {
-      mainWindow.show()
-    }
-  })
+app.on('window-all-closed', function () {
+  app.quit()
 })
